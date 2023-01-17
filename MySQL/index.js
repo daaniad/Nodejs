80a3ea8f39ea50b66e986f2b3163530e70a83617
Daniel Valderrama Arrabal <danivva95@gmail.com>
2023-01-17 15:22:59 +0100

    modified index js with cors

--- MySQL/index.js
+++ MySQL/index.js
@@ -4,6 +4,7 @@ import logger from "morgan";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user_router.js";
 import productRouter from "./routes/produtcs_router.js";
+import cors from "cors"
 
 // Añadimos el método config de dotenv para utilizar las variables de entorno
 dotenv.config();
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(express.text());
 app.use(logger("dev"));
 app.use(cookieParser());
+app.use(cors())
 app.use("/user", userRouter)
 
 //await db.createConnection();
